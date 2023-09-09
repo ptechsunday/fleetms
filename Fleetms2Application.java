5f14a1ebccbe100a38b5ff14b591fedc305432e4
Atoh Sunday <atohsun4all@gmail.com>
2023-09-09 15:32:10 +0100

    first commit

---
+++ Fleetms2Application.java
@@ -0,0 +1,13 @@
+package com.suntech.fleetms2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fleetms2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fleetms2Application.class, args);
+	}
+
+}
